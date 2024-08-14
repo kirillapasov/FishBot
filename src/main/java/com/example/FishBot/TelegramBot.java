850b91f70ef306b0d1078f9e736eeea16fc6566f
Kirill Apasov <kirill.apasov@yandex.ru>
2024-08-14 19:08:10 +0300

    add handleLocationCommand logic in TelegramBot.java

--- src/main/java/com/example/FishBot/TelegramBot.java
+++ src/main/java/com/example/FishBot/TelegramBot.java
@@ -4,6 +4,7 @@ import com.example.FishBot.config.BotConfig;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendLocation;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -29,20 +30,19 @@ public class TelegramBot extends TelegramLongPollingBot {
         if(update.hasMessage() && update.getMessage().hasText()){
             String messageText = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
-
-            switch (messageText){
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                default:
-
-                    sendMessage(chatId, "САСИ ПИДОРОК");
+            if (messageText.equals("/start")) {
+                startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
             }
+            else if (messageText.startsWith("/location")) {
+                handleLocationCommand(chatId, messageText);
+            }
+            else
+                sendMessage(chatId, "Введите коректную команду");
         }
     }
 
     private void startCommandReceived(Long chatId, String name) {
-        String answer = "ЗДАРОВА ИДИ НАХУЦ";
+        String answer = "Здравствуйте, используйте команду /location <Широта> <Долгота>";
         sendMessage(chatId, answer);
     }
 
@@ -52,6 +52,34 @@ public class TelegramBot extends TelegramLongPollingBot {
         sendMessage.setText(textToSend);
         try {
             execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    private void handleLocationCommand(long chatId, String messageText) {
+        try {
+            String[] parts = messageText.split(" ");
+            if (parts.length == 3) {
+                float latitude = Float.parseFloat(parts[1]);
+                float longitude = Float.parseFloat(parts[2]);
+
+                sendLocation(chatId, latitude, longitude);
+            } else {
+                sendMessage(chatId, "Введите команду в формате: /location <широта> <долгота>");
+            }
+        } catch (NumberFormatException e) {
+            sendMessage(chatId, "Неверный формат координат. Попробуйте снова.");
+        }
+    }
+
+    private void sendLocation(long chatId, double latitude, double longitude) {
+        SendLocation locationMessage = new SendLocation();
+        locationMessage.setChatId(chatId);
+        locationMessage.setLatitude(latitude);
+        locationMessage.setLongitude(longitude);
+
+        try {
+            execute(locationMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
